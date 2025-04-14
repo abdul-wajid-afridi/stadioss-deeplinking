8636e31af08231e9b52ba4def0ebcb0fdc977995
Abdul Wajid Afridi <wajidafridi8554@gmail.com>
2025-04-14 13:31:21 +0500

    Add facebook smart banner

--- app/root.tsx
+++ app/root.tsx
@@ -26,6 +26,20 @@ export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
+        <meta name="apple-itunes-app" content="app-id=284882215" />
+        <meta name="google-play-app" content="app-id=com.facebook.katana" />
+        <meta
+          name="msApplication-ID"
+          content="82a23635-5bd9-df11-a844-00237de2db9e"
+        />
+        <meta
+          name="msApplication-PackageFamilyName"
+          content="facebook_9wzdncrfhv5g"
+        />
+        <link rel="apple-touch-icon" href="icon.png" />
+        <link rel="android-touch-icon" href="icon.png" />
+        <link rel="windows-touch-icon" href="icon.png" />
+
         <title>fix user phones</title>
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
